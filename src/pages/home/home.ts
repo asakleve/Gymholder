fd95fae4a1283f43ee1a60a81f05f28271e27a1a
Marialindstrom <marialindstrom92@hotmail.com>
2017-05-09 12:01:28 +0200

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {MapPage} from '../map/map';
+import {EventLeaderboardPage} from '../eventLeaderboard/eventLeaderboard';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,12 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+openMapPage(){
+this.navCtrl.setRoot(MapPage);
+}
+
+openEventLeaderboardPage(){
+this.navCtrl.setRoot(EventLeaderboardPage);
+}
 
 }
