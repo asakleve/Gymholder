68081639bab4bc15a604fd54e423c036860c5822
Christian Rusch <christianrusch.su@gmail.com>
2017-05-09 13:53:33 +0200

    halfway

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  email = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public nav: NavController, private auth: AuthService) {
+    // let info = this.auth.getUserInfo();
+    // this.email = info['email'];
+  }
 
+  public logout() {
+    this.auth.logout().subscribe(succ => {
+      this.nav.setRoot('LoginPage')
+    });
   }
 
 }
