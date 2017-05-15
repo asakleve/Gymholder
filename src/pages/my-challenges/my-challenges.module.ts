19e3b7a7b80699bd87607c943ec20de78e8fe6d6
Åsa Kleve <awesome@eduroam-193-11-94-172.public.su.se>
2017-05-15 10:42:43 +0200

    Inga ändringar e.g.

--- src/pages/my-challenges/my-challenges.module.ts
+++ src/pages/my-challenges/my-challenges.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MyChallengesPage } from './my-challenges';
 
+
 @NgModule({
   declarations: [
     MyChallengesPage,
