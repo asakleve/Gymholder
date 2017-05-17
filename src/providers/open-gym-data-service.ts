23188a3e5161581ddc2a7b7656de61687b49d0c9
Christian Rusch <christianrusch.su@gmail.com>
2017-05-17 11:48:24 +0200

    api.stockholm.se -> ny backend-metod för hämtning av specifika gymdata

--- src/providers/open-gym-data-service.ts
+++ src/providers/open-gym-data-service.ts
@@ -13,6 +13,7 @@ import 'rxjs/add/operator/map';
 export class OpenGymDataService {
 
   public openData: any;
+  public ourApiKey = 'ffd5810dd624442ca44452e55964fd67';
 
   constructor(public http: Http) {
 
@@ -20,7 +21,12 @@ export class OpenGymDataService {
 
   public loadGymData() {
     // http://api.stockholm.se/ServiceGuideService/ServiceUnitTypes/96a67da3-938b-487e-ac34-49b155cb277b/ServiceUnits/json?apikey=ffd5810dd624442ca44452e55964fd67
-    return this.http.get('/openStockholm/ServiceGuideService/ServiceUnitTypes/96a67da3-938b-487e-ac34-49b155cb277b/ServiceUnits/json?apikey=ffd5810dd624442ca44452e55964fd67')
+    return this.http.get('/openStockholm/ServiceGuideService/ServiceUnitTypes/96a67da3-938b-487e-ac34-49b155cb277b/ServiceUnits/json?apikey=' + this.ourApiKey)
+      .map(res => res.json());
+  }
+
+  public loadGymDetails(gymid: string) {
+    return this.http.get('/openStockholm/ServiceGuideService/ServiceUnits/' + gymid + '/json?apikey=' + this.ourApiKey)
       .map(res => res.json());
   }
 }
