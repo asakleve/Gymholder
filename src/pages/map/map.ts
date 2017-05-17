84bf43a493c93004b52aaa7a0af5018e390d1def
Åsa Kleve <awesome@eduroam-130-237-246-24.public.su.se>
2017-05-17 08:57:37 +0200

    design fixar

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -87,8 +87,8 @@ export class MapPage {
       position: new google.maps.LatLng(x, y)
     });
     marker.infoWindow = new google.maps.InfoWindow({
-      content: '<p class="infoWinHeader">' + name + '</p></br>'
-      + '<p class="infoWinBody">Here be text and shit. Arrr.</p>'
+      content: '<p class="infoWinHeader">' + name + '</p></br>'+
+      '<p class="infoWinBody">Here be text and shit. Arrr.</p>'
     });
     google.maps.event.addListener(marker, 'click', () => {
       if(this.lastMarker != null) {
@@ -105,6 +105,10 @@ export class MapPage {
     });
 
 
+  }
+
+  openMy(){
+  console.log('Hej');
   }
 
 }
