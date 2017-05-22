c0450b2ad7d784a72138bd2edaf43b60a11fdce5
Jacqueline Tan <jacqueline@eduroam-193-11-94-93.public.su.se>
2017-05-22 12:15:43 +0200

    SettingsPage

--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -19,23 +19,7 @@ export class FriendsPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Friends');
-  }
-
-
-  getItems(ev) {
-    // Reset items back to all of the items
-    
-
-    // set val to the value of the ev target
-    var val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
     }
-  }
 
 
 }
